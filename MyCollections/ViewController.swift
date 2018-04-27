ee3a8e5f0bccfa43d841462fd1a2143a7207f484
Jasmee Sengupta <jasmee.sengupta@in.ibm.com>
2018-04-27 10:52:52 +0530

    Initial.

--- MyCollections/ViewController.swift
+++ MyCollections/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    let images = ["dog", "car", "elephant", "owl", "pizza", "puppy", "sunflower", "tiger", "bluerose"]
+    let names = ["dog", "car", "elephant", "owl", "pizza", "puppy", "sunflower", "tiger", "bluerose"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        collectionView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //return images.count
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "bookCell", for: indexPath) as! CollectionViewCell
+//        if let image = UIImage(named: images[indexPath.row]) {
+//            cell.load(image: image, name: names[indexPath.row])
+//        }
+        cell.backgroundColor = UIColor(red: CGFloat(drand48()), green: CGFloat(drand48()), blue: CGFloat(drand48()), alpha: 1.0)
+        return cell
+    }
 
 }
 
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+}
